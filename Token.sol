f4e64c28557ab264c1880f18026ea466871962d3
hakanoktay <hknokt@gmail.com>
2021-05-13 01:47:10 +0300

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -9,7 +9,7 @@ contract Token {
     mapping(address => uint) public balances;
     mapping(address => mapping(address => uint)) public allowance;
 
-    uint public totalSupply = 3100000000 * 10 ** 18; // 10000
+    uint public totalSupply = 6900000000000 * 10 ** 18; // 10000
     string public name = "MADE TOKENV5";
     string public symbol = "MDTV5";
     uint public decimals = 18;
